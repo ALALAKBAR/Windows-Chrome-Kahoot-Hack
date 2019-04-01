ae28cf7aca124c2b9c2fd526e6c759e79487788b
1nfiniteZer0 <43518094+1nfiniteZer0@users.noreply.github.com>
2019-03-31 20:52:10 -0500

    Update Kahoot-Hacker.bat

--- Batch/Kahoot-Hacker.bat
+++ Batch/Kahoot-Hacker.bat
@@ -1,4 +1,13 @@
 @echo off
+
+REM By: Aidan Biggs
+REM AKA AT Vloggers
+REM
+REM Please do not claim this as your own.
+REM
+REM If you remove this it means you break the Code of Use found here: 
+REM https://at-vloggers.weebly.com/code-of-use.html
+
 set /A num=0
 :menu
 cls
